002d7d778da34e160e12253ed46dd704bcb77aa7
Antoine Briat <antoinebriat0@gmail.com>
2025-01-13 21:59:36 +0100

    modif

--- loader.js
+++ loader.js
@@ -3,5 +3,5 @@ window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     setTimeout(() => {
         loader.style.display = 'none'; // Cache le loader
-    }, 1000); // Temps d'affichage du loader (2 secondes)
+    }, 500); // Temps d'affichage du loader
 });
